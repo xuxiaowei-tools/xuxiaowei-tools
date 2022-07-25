70016b549373e5b35c9613b0175e13e9d807c52d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-07-25 11:28:09 +0800

    :sparkles: 新增秘钥编码后进行加解密，新增偏移量编码后进行加解密，新增解密后的结果进行编码

--- src/views/encryption/AESView.vue
+++ src/views/encryption/AESView.vue
@@ -16,7 +16,7 @@
 
   <br>
 
-  <el-row justify="space-evenly" v-if="type === '2'">
+  <el-row justify="space-evenly">
     <el-col :span="11">
       <el-select v-model="mode" placeholder="请选择模式" class="w-100%">
         <el-option v-for="item in modeOptions" :key="item.value" :label="item.label" :value="item.value"/>
@@ -37,12 +37,22 @@
         <template #prepend>
           <span>秘钥 key</span>
         </template>
+        <template #append>
+          <el-select v-model="keyMode" placeholder="选择秘钥编码">
+            <el-option v-for="item in modeEncOptions" :key="item.value" :label="item.label" :value="item.value"/>
+          </el-select>
+        </template>
       </el-input>
     </el-col>
     <el-col :span="11" v-if="type === '2'">
       <el-input v-model="iv" placeholder="请输入偏移量" clearable @dblclick="dblclick" data-dblclick="偏移量已复制到剪贴板">
         <template #prepend>
           <span>偏移量 iv</span>
+        </template>
+        <template #append>
+          <el-select v-model="ivMode" placeholder="选择偏移量编码">
+            <el-option v-for="item in ivEncOptions" :key="item.value" :label="item.label" :value="item.value"/>
+          </el-select>
         </template>
       </el-input>
     </el-col>
@@ -56,6 +66,16 @@
     </el-col>
     <el-col :span="11">
       <el-button @click="decrypt" class="decrypt-button w-100%">解密</el-button>
+    </el-col>
+  </el-row>
+
+  <br>
+
+  <el-row justify="space-evenly">
+    <el-col :span="11">
+      <el-select v-model="decryptEnc" placeholder="请选择解密编码" class="w-100%">
+        <el-option v-for="item in decryptEncOptions" :key="item.value" :label="item.label" :value="item.value"/>
+      </el-select>
     </el-col>
   </el-row>
 
@@ -92,12 +112,12 @@ const { toClipboard } = useClipboard()
 const type = ref((route.query.type === '1' || route.query.type === '2') ? route.query.type : '1')
 
 // 模式
-const mode = ref('CBC')
+const mode = ref('ECB')
 // https://github.com/brix/crypto-js/blob/develop/docs/QuickStartGuide.wiki#block-modes-and-padding
 const modeOptions = [
   {
     value: 'CBC',
-    label: 'CBC (default)'
+    label: 'CBC'
   },
   {
     value: 'CFB',
@@ -123,7 +143,7 @@ const padding = ref('Pkcs7')
 const paddingOptions = [
   {
     value: 'Pkcs7',
-    label: 'Pkcs7 (default)'
+    label: 'Pkcs7'
   },
   {
     value: 'Iso97971',
@@ -143,11 +163,126 @@ const paddingOptions = [
   }
 ]
 
+// 解密编码
+const decryptEnc = ref('Utf8')
+const decryptEncOptions = [
+  {
+    value: 'Utf8',
+    label: 'Utf8'
+  },
+  {
+    value: 'Hex',
+    label: 'Hex'
+  },
+  {
+    value: 'Latin1',
+    label: 'Latin1'
+  },
+  {
+    value: 'Utf16',
+    label: 'Utf16'
+  },
+  {
+    value: 'Utf16BE',
+    label: 'Utf16BE'
+  },
+  {
+    value: 'Utf16LE',
+    label: 'Utf16LE'
+  },
+  {
+    value: 'Base64',
+    label: 'Base64'
+  },
+  {
+    value: 'Base64url',
+    label: 'Base64url'
+  }
+]
+
 // 秘钥
-const key = ref('secret key 123')
+const key = ref('1234567890ABCDEF')
+const keyMode = ref('Utf8')
+const modeEncOptions = [
+  {
+    value: '',
+    label: 'default'
+  },
+  {
+    value: 'Utf8',
+    label: 'Utf8'
+  },
+  {
+    value: 'Hex',
+    label: 'Hex'
+  },
+  {
+    value: 'Latin1',
+    label: 'Latin1'
+  },
+  {
+    value: 'Utf16',
+    label: 'Utf16'
+  },
+  {
+    value: 'Utf16BE',
+    label: 'Utf16BE'
+  },
+  {
+    value: 'Utf16LE',
+    label: 'Utf16LE'
+  },
+  {
+    value: 'Base64',
+    label: 'Base64'
+  },
+  {
+    value: 'Base64url',
+    label: 'Base64url'
+  }
+]
 
 // 偏移量
 const iv = ref('')
+const ivMode = ref('Utf8')
+const ivEncOptions = [
+  {
+    value: '',
+    label: 'default'
+  },
+  {
+    value: 'Utf8',
+    label: 'Utf8'
+  },
+  {
+    value: 'Hex',
+    label: 'Hex'
+  },
+  {
+    value: 'Latin1',
+    label: 'Latin1'
+  },
+  {
+    value: 'Utf16',
+    label: 'Utf16'
+  },
+  {
+    value: 'Utf16BE',
+    label: 'Utf16BE'
+  },
+  {
+    value: 'Utf16LE',
+    label: 'Utf16LE'
+  },
+  {
+    value: 'Base64',
+    label: 'Base64'
+  },
+  {
+    value: 'Base64url',
+    label: 'Base64url'
+  }
+]
 
 // 原文
 const originalText = ref('my message')
@@ -157,40 +292,78 @@ const ciphertext = ref('')
 
 // 加密
 const encrypt = () => {
-  if (type.value === '1') {
-    // 1：仅秘钥 key
-    ciphertext.value = CryptoJS.AES.encrypt(originalText.value, key.value).toString()
-  } else {
-    // 2：秘钥 key + 偏移量 iv
-    const ivParse = CryptoJS.enc.Latin1.parse(iv.value)
-    ciphertext.value = CryptoJS.AES.encrypt(originalText.value, key.value, {
-      ivParse,
-      mode: getMode(mode.value),
-      padding: getPadding(padding.value)
-    }).toString()
+  const keyValue = valueParse(keyMode.value, key.value)
+  try {
+    if (type.value === '1') {
+      // 1：仅秘钥 key
+      ciphertext.value = CryptoJS.AES.encrypt(originalText.value, keyValue, {
+        mode: getMode(mode.value),
+        padding: getPadding(padding.value)
+      }).toString()
+    } else {
+      // 2：秘钥 key + 偏移量 iv
+      const ivParse = valueParse(ivMode.value, iv.value)
+      ciphertext.value = CryptoJS.AES.encrypt(originalText.value, keyValue, {
+        ivParse,
+        mode: getMode(mode.value),
+        padding: getPadding(padding.value)
+      }).toString()
+    }
+    ElMessage({ message: '加密完成', type: 'success' })
+  } catch (e) {
+    if (type.value === '1') {
+      ElMessage.error('加密失败，请确认秘钥 key、模式 mode、填充 padding 是否正确')
+    } else {
+      ElMessage.error('加密失败，请确认秘钥 key、偏移量 iv、模式 mode、填充 padding 是否正确')
+    }
+    console.error(e)
   }
 }
 
 // 解密
 const decrypt = () => {
   let bytes
-  if (type.value === '1') {
-    // 1：仅秘钥 key
-    bytes = CryptoJS.AES.decrypt(ciphertext.value, key.value)
-  } else {
-    // 2：秘钥 key + 偏移量 iv
-    const ivParse = CryptoJS.enc.Latin1.parse(iv.value)
-    bytes = CryptoJS.AES.decrypt(ciphertext.value, key.value,
-      {
-        ivParse,
+  const keyValue = valueParse(keyMode.value, key.value)
+  try {
+    if (type.value === '1') {
+      // 1：仅秘钥 key
+      bytes = CryptoJS.AES.decrypt(ciphertext.value, keyValue, {
         mode: getMode(mode.value),
         padding: getPadding(padding.value)
       })
+    } else {
+      // 2：秘钥 key + 偏移量 iv
+      const ivParse = CryptoJS.enc.Latin1.parse(iv.value)
+      bytes = CryptoJS.AES.decrypt(ciphertext.value, keyValue,
+        {
+          ivParse,
+          mode: getMode(mode.value),
+          padding: getPadding(padding.value)
+        })
+    }
+    ElMessage({ message: '解密完成', type: 'success' })
+  } catch (e) {
+    if (type.value === '1') {
+      ElMessage.error('解密失败，请确认秘钥 key、模式 mode、填充 padding 是否正确')
+    } else {
+      ElMessage.error('解密失败，请确认秘钥 key、偏移量 iv、模式 mode、填充 padding 是否正确')
+    }
+    console.error(e)
   }
-  originalText.value = bytes.toString(CryptoJS.enc.Utf8)
+
+  if (bytes === undefined) {
+    return
+  }
+
+  try {
+    originalText.value = bytes.toString(getEnc(decryptEnc.value))
+  } catch (e) {
+    ElMessage.error('解密后转码失败')
+    console.error(e)
+  }
 }
 
-// 转译
+// 转译模式
 const getMode = (mode: string) => {
   switch (mode) {
     case 'CBC':
@@ -206,7 +379,7 @@ const getMode = (mode: string) => {
   }
 }
 
-// 转译
+// 转译填充
 const getPadding = (padding: string) => {
   switch (padding) {
     case 'Pkcs7':
@@ -219,6 +392,52 @@ const getPadding = (padding: string) => {
       return CryptoJS.pad.Iso10126
     case 'ZeroPadding':
       return CryptoJS.pad.ZeroPadding
+  }
+}
+
+// 编码
+const valueParse = (enc: string, value: string): any => {
+  switch (enc) {
+    case '':
+      return value
+    case 'Hex':
+      return CryptoJS.enc.Hex.parse(value)
+    case 'Latin1':
+      return CryptoJS.enc.Latin1.parse(value)
+    case 'Utf8':
+      return CryptoJS.enc.Utf8.parse(value)
+    case 'Utf16':
+      return CryptoJS.enc.Utf16.parse(value)
+    case 'Utf16BE':
+      return CryptoJS.enc.Utf16BE.parse(value)
+    case 'Utf16LE':
+      return CryptoJS.enc.Utf16LE.parse(value)
+    case 'Base64':
+      return CryptoJS.enc.Base64.parse(value)
+    case 'Base64url':
+      return CryptoJS.enc.Base64url.parse(value)
+  }
+}
+
+// 转译编码
+const getEnc = (enc: string) => {
+  switch (enc) {
+    case 'Hex':
+      return CryptoJS.enc.Hex
+    case 'Latin1':
+      return CryptoJS.enc.Latin1
+    case 'Utf8':
+      return CryptoJS.enc.Utf8
+    case 'Utf16':
+      return CryptoJS.enc.Utf16
+    case 'Utf16BE':
+      return CryptoJS.enc.Utf16BE
+    case 'Utf16LE':
+      return CryptoJS.enc.Utf16LE
+    case 'Base64':
+      return CryptoJS.enc.Base64
+    case 'Base64url':
+      return CryptoJS.enc.Base64url
   }
 }
 
