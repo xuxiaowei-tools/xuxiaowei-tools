e4be045ba4881632ab79e8d02e2afb83f2444750
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-07-27 23:28:54 +0800

    :sparkles: 使用 keypair 生成 RSA 密钥对

--- src/views/encryption/RSAView.vue
+++ src/views/encryption/RSAView.vue
@@ -3,7 +3,11 @@
 </template>
 
 <script setup lang="ts">
+import keypair from 'keypair'
 
+const pair = keypair({ bits: 2048 }) // 默认 2048
+console.log(pair.public)
+console.log(pair.private)
 </script>
 
 <style scoped>
