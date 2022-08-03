48fb7531e75cc01f2fbe961edfc972921d98bb90
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-08-03 19:08:36 +0800

    :sparkles: BCrypt比较密码

--- src/views/encryption/BCryptView.vue
+++ src/views/encryption/BCryptView.vue
@@ -5,6 +5,15 @@
     </el-col>
   </el-row>
 
+  <el-row class="text-center">
+    <el-col :span="22">
+      <el-radio-group v-model="prefix">
+        <el-radio border label="">无</el-radio>
+        <el-radio border :label="bcryptPrefix">{{bcryptPrefix}}}</el-radio>
+      </el-radio-group>
+    </el-col>
+  </el-row>
+
   <br>
 
   <el-row class="button-row" justify="center" :gutter="20">
@@ -12,7 +21,7 @@
       <el-button @click="encrypt" class="encrypt-button w-100%">加密</el-button>
     </el-col>
     <el-col :span="11">
-
+      <el-button @click="compare" class="encrypt-button w-100%">比较</el-button>
     </el-col>
   </el-row>
 
@@ -31,10 +40,14 @@
 </template>
 
 <script setup lang="ts">
-import bcrypt from 'bcryptjs'
+import bcryptjs from 'bcryptjs'
 import { ElMessage } from 'element-plus/es'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import useClipboard from 'vue-clipboard3'
+import { LocationQueryRaw, useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
 
 const { toClipboard } = useClipboard()
 
@@ -44,11 +57,57 @@ const originalText = ref('my message')
 // 密文
 const ciphertext = ref('')
 
+// 前缀
+const prefix = ref('')
+watch(() => prefix.value, (newValue, oldValue) => {
+  // 切换填充方案时，改变 URL 参数
+  const query = JSON.parse(JSON.stringify(route.query))
+  query.prefix = newValue
+  const locationQueryRaw = ref<LocationQueryRaw>(query)
+
+  router.replace({
+    query: locationQueryRaw.value
+  })
+})
+
+// Security 前缀
+const bcryptPrefix = ref('{bcrypt}')
+
 // 加密
 const encrypt = () => {
-  const salt = bcrypt.genSaltSync(10)
-  ciphertext.value = bcrypt.hashSync(originalText.value, salt)
+  const salt = bcryptjs.genSaltSync(10)
+  const hash = bcryptjs.hashSync(originalText.value, salt)
+  ciphertext.value = prefix.value === '' ? hash : prefix.value + hash
+  ElMessage({ message: '加密完成', type: 'success' })
 }
+
+const compare = () => {
+  let compare
+  if (ciphertext.value.startsWith(bcryptPrefix.value)) {
+    compare = bcryptjs.compareSync(originalText.value, ciphertext.value.substring(bcryptPrefix.value.length))
+  } else {
+    compare = bcryptjs.compareSync(originalText.value, ciphertext.value)
+  }
+  if (compare) {
+    ElMessage({ message: '密码与原文匹配', type: 'success' })
+  } else {
+    ElMessage.error('密码与原文不匹配')
+  }
+}
+
+router.isReady().then(() => {
+  const query = JSON.parse(JSON.stringify(route.query))
+  const queryPrefix = query.prefix
+
+  if (queryPrefix !== undefined) {
+    prefix.value = queryPrefix
+  }
+
+  const prefixList = ref<string[]>(['', bcryptPrefix.value])
+  if (prefixList.value.indexOf(prefix.value) === -1) {
+    prefix.value = ''
+  }
+})
 
 // 双击复制
 const dblclick = async (e: any) => {
