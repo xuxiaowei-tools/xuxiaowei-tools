02bdaf5c96d7e99747771bbffafcd9f270e503c2
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-05-02 14:56:44 +0800

    :sparkles: docker

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
